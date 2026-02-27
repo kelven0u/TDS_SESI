448eb0fa58fa1a7ef3ad683ce412f9fdf70a152f
kelven0u <kelvendossantossteinheuser@gmail.com>
2026-02-27 11:48:27 -0300

    tipos de dados js

---
+++ inicio/dados2.js
@@ -0,0 +1,13 @@
+// console.log (19.9 * 0.6);
+
+var preco = 20;
+var desconto = 0.6;
+var precocomdesconto = preco * (1 - desconto);
+console.log(precocomdesconto);
+// console.log(precocomdesconto);
+
+let nome = "caderno";
+let categoria = "papelaria";
+console.log("produto:" + nome + " categoria: "
+ + categoria + "desconto: "
+  + desconto + "preco: " + preco);
---
+++ inicio/dados3.js
@@ -0,0 +1,17 @@
+let idade = 24;
+console.log(typeof idade);
+console.log(typeof 24);
+
+ let valor = 1500.50;
+
+ console.log(typeof valor);
+ let estachovendo = false; // true
+ console.log(typeof estachovendo);
+
+ let nome = "kelven";
+ console.log(typeof valor);
+
+ console.log(1 + 1);
+ console.log("1" + 1);
+
+ console.log(valor * "valor")
