48a77b52ce9af9784bb5106b3992964a8a730c71
kelven0u <kelvendossantossteinheuser@gmail.com>
2026-03-06 11:40:01 -0300

    Operadores de atribuição e relacionais

---
+++ inicio/operadores/atribuicao.js
@@ -0,0 +1,19 @@
+let x = 100;
+x = x + 5;
+x +=5;
+x -=5;
+x *=5; // 525
+x /=5; // 105
+x %=5; // 0
+
+console.log()
+
+let y = 200;
+y = y + 100;
+y += 100;
+console.log(y);
+
+let nome = "kelven";
+nome += " dos santos";
+
+console.log(nome);
